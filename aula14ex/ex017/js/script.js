a0b9db2efea1490eaa65a4a04aedb4f63693d0b1
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-07-04 21:38:54 -0300

    Exercio Tabuada

--- aula14ex/ex017/js/script.js
+++ aula14ex/ex017/js/script.js
@@ -1,1 +1,32 @@
-alert("Olar")
+window.onload = () => {
+    document.getElementById('btn').addEventListener('click', tabuada)
+}
+
+function tabuada() {
+
+    let mult = document.querySelector('#numTab').valueAsNumber
+    let result = document.querySelector('#resultado')
+
+    if (isNaN(mult)) {
+        alert('Selecione um numero')
+
+    } else {
+        result.innerHTML = ''
+
+        for (let i = 0; i <= 10; i++) {
+            let opt = document.createElement('option')
+            opt.text = `${mult} X ${i} = ${i*mult}`
+            result.appendChild(opt)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
